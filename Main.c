c790a33e6c62167c752020880807c893fd254481
Joe Belford <joebelford@gmail.com>
2022-11-19 16:45:28 +0000

    refactored to soap_srv

--- Main.c
+++ Main.c
@@ -4,16 +4,16 @@
 int main()
 {
   fprintf(stdout,"Starting...\n");
-  struct soap *soap = soap_new1(SOAP_XML_INDENT);
-  if (!soap_valid_socket(soap_bind(soap, NULL, 8080, 10))) // port 8080 and small BACKLOG queue for iterative servers
+  struct soap *soap_srv = soap_new1(SOAP_XML_INDENT);
+  if (!soap_valid_socket(soap_bind(soap_srv, NULL, 8080, 10))) // port 8080 and small BACKLOG queue for iterative servers
     exit(EXIT_FAILURE);
-  while (soap_valid_socket(soap_accept(soap)))
+  while (soap_valid_socket(soap_accept(soap_srv)))
   {
-    if (soap_serve(soap) != SOAP_OK)
+    if (soap_serve(soap_srv) != SOAP_OK)
       break;
-    soap_destroy(soap); // delete deserialized C++ instances
-    soap_end(soap);     // delete other managed data
+    soap_destroy(soap_srv); // delete deserialized C++ instances
+    soap_end(soap_srv);     // delete other managed data
   }
-  soap_print_fault(soap, stderr);
-  soap_free(soap); // free the soap struct context data
+  soap_print_fault(soap_srv, stderr);
+  soap_free(soap_srv); // free the soap struct context data
 }
