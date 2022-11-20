94d93057552d7c454b6ce9d4015ffdc30b327ea8
Joe Belford <joebelford@gmail.com>
2022-11-20 13:31:16 +0000

    multithread runner

--- Main.c
+++ Main.c
@@ -1,110 +1,68 @@
-// #include "PullPointSubscriptionBinding.nsmap" // XML namespace mapping table (only needed once at the global level)
-// #include "soapH.h"                            // server stubs, serializers, etc.
-
-// int main()
-// {
-//   fprintf(stdout,"Starting...\n");
-//   struct soap *soap_srv = soap_new1(SOAP_XML_INDENT);
-//   if (!soap_valid_socket(soap_bind(soap_srv, NULL, 8080, 10))) // port 8080 and small BACKLOG queue for iterative servers
-//     exit(EXIT_FAILURE);
-//   while (soap_valid_socket(soap_accept(soap_srv)))
-//   {
-//     if (soap_serve(soap_srv) != SOAP_OK)
-//       break;
-//     soap_destroy(soap_srv); // delete deserialized C++ instances
-//     soap_end(soap_srv);     // delete other managed data
-//   }
-//   soap_print_fault(soap_srv, stderr);
-//   soap_free(soap_srv); // free the soap struct context data
-// }
 #include "PullPointSubscriptionBinding.nsmap" // XML namespace mapping table (only needed once at the global level)
 #include "soapH.h"
 #include "plugin/threads.h"
 
-#define PORTNUM 8080
-#define BACKLOG 10
-
-void * accept_request(struct soap*);
-void * process_request(struct soap*);
-void cleanup(struct soap*);
-
-int main()
+#define BACKLOG (100) // Max request backlog of pending requests
+int main(int argc, char **argv) 
 {
-  struct soap *soap = soap_new();
-  soap->bind_flags = SO_REUSEADDR; // immediate port reuse
-  soap->accept_timeout = 3600;     // exit loop when no request arrives in one hour
-  soap->send_timeout = 5;          // 5 seconds max socket stall time (unlimited by default)
-  soap->recv_timeout = 5;
-  soap->transfer_timeout = 30;    // 30 seconds max message transfer time (unlimited by default)
-  soap->recv_maxlength = 1048576; // limit messages received to 1MB (2GB by default)
-  if (soap_valid_socket(soap_bind(soap, NULL, PORTNUM, BACKLOG)))
+  struct soap soap; 
+  soap_init(&soap); 
+  if (argc < 2) // no args: assume this is a CGI application 
   {
+    soap_serve(&soap);   // serve request, one thread, CGI style 
+    soap_destroy(&soap); // delete managed class instances 
+    soap_end(&soap);     // delete managed data and temporaries 
+  } 
+  else 
+  {
+    void *process_request(void*); 
+    struct soap *tsoap; 
+    THREAD_TYPE tid; 
+    int port = atoi(argv[1]); // first command-line arg is port 
+    SOAP_SOCKET m, s; 
+    soap.send_timeout = 10; // 10 seconds max socket delay 
+    soap.recv_timeout = 10; // 10 seconds max socket delay 
+    soap.accept_timeout = 3600; // server stops after 1 hour of inactivity 
+    soap.max_keep_alive = 100; // max keep-alive sequence 
+    m = soap_bind(&soap, NULL, port, BACKLOG); 
+    if (!soap_valid_socket(m)) 
+      exit(EXIT_FAILURE); 
+    fprintf(stderr, "Socket connection successful %d\n", m); 
     while (1)
     {
-      if (soap_valid_socket(soap_accept(soap)))
+      s = soap_accept(&soap); 
+      if (soap_valid_socket(s)) 
       {
-        THREAD_TYPE tid;
-        struct soap *tsoap = soap_copy(soap);
-        if (!tsoap)
-          soap_force_closesock(soap);
+        fprintf(stderr, "Accept socket %d connection from IP %d.%d.%d.%d\n", s, (soap.ip>>24)&0xFF, (soap.ip>>16)&0xFF, (soap.ip>>8)&0xFF, soap.ip&0xFF); 
+        tsoap = soap_copy(&soap); // make a copy 
+        if (!tsoap) 
+          soap_force_closesock(&soap);
         else
-          while (THREAD_CREATE(&tid, (void *(*)(void *))&accept_request, (void *)tsoap))
+          while (THREAD_CREATE(&tid, (void*(*)(void*))process_request, (void*)tsoap))
             sleep(1); // failed, try again
       }
-      else if (soap->errnum) // accept failed, try again after 1 second
+      else if (soap.errnum) // accept failed, try again after 1 second
       {
-        soap_print_fault(soap, stderr);
+        soap_print_fault(&soap, stderr); 
         sleep(1);
-      }
-      else // accept_timeout timed out, quit looping
+      } 
+      else
       {
-        break;
-      }
-      soap_destroy(soap);
-      soap_end(soap);
-    }
-  }
-  soap_free(soap);
-}
-void *accept_request(struct soap *soap)
+        fprintf(stderr, "server timed out\n"); 
+        break; 
+      } 
+    } 
+  } 
+  soap_done(&soap); // finalize context
+  return 0; 
+} 
+void *process_request(void* tsoap) 
 {
-  THREAD_TYPE tid;
-  struct soap *tsoap;
-  THREAD_DETACH(THREAD_ID);
-  // create a new thread that is timed to execute for max 10 seconds
-  tsoap = soap_copy(soap);
-  if (!tsoap)
-  {
-    soap_force_closesock(soap);
-  }
-  else
-  {
-    while (THREAD_CREATE(&tid, (void *(*)(void *))&process_request, (void *)tsoap))
-      sleep(1); // failed, try again
-    // allow serving the request by the new thread for up to 30 seconds max
-    sleep(30);
-    // terminate process_request thread
-    THREAD_CANCEL(tid);
-  }
-  // clean up
-  soap_destroy(soap);
-  soap_end(soap);
-  soap_free(soap);
-  return NULL;
-}
-void *process_request(struct soap *soap)
-{
-  fprintf(stdout, "Processing...\n");
-  // add the cleanup function
-  pthread_cleanup_push((void (*)(void *))&cleanup, (void *)soap);
-  soap_serve(soap);
-  // remove the cleanup function and call it to cleanup the context
-  pthread_cleanup_pop(1);
-  return NULL;
-}
-void cleanup(struct soap *soap)
-{
-  soap_destroy(soap);
-  soap_end(soap);
-  soap_free(soap);
+  struct soap *soap = (struct soap*)tsoap;
+  THREAD_DETACH(THREAD_ID); 
+  soap_serve(soap); 
+  soap_destroy(soap); // delete managed class instances 
+  soap_end(soap);     // delete managed data and temporaries 
+  soap_free(soap);    // finalize and delete the context
+  return NULL; 
 }
